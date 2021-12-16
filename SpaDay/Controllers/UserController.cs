f6bae7b6f946f805716c427c69a59b1b44a4cad4
Adam Bailey <abailey76@duck.com>
2021-12-15 20:20:03 -0600

    studio 14.6 complete through bonus missions

---
+++ SpaDay/Controllers/UserController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SpaDay.Models;
+
+namespace SpaDay.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        [Route("/User/Add")]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("/User/Add")]
+        public IActionResult SubmitAddUserForm(User newUser, string verify)
+        {
+            if(newUser.Password == verify)
+            {
+                ViewBag.user = newUser;
+                return View("Index");
+            }
+            else
+            {
+                ViewBag.userName = newUser.UserName;
+                ViewBag.Email = newUser.Email;
+                ViewBag.error = "Your passwords should match!";
+                return View("Add");
+            }
+            
+        }
+
+
+    }
+}
---
+++ SpaDay/Models/User.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace SpaDay.Models
+{
+    public class User
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public DateTime DateCreated { get; }
+
+        public User()
+        {
+            DateCreated = DateTime.Now;
+        }
+
+        public User(string userName, string password, string email) : this()
+        {
+            UserName = userName;
+            Password = password;
+            Email = email;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return base.Equals(obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return $"userName: {UserName} email: {Email}";
+        }
+    }
+}
