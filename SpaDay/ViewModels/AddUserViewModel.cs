8577cb846b4cc149274566b33c98a11de0f64530
Adam Bailey <abailey76@duck.com>
2021-12-21 11:18:22 -0600

    working through studio while amelia laughs

---
+++ SpaDay/ViewModels/AddUserViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SpaDay.ViewModels
+{
+    public class AddUserViewModel
+    {
+        [Required(ErrorMessage = "Username is required.")]
+        public string Username { get; set; }
+        
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        [StringLength(20, MinimumLength = 6, ErrorMessage = "Password should be between 6 and 20 characters.")]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Password and Verify must match!")]
+        public string VerifyPassword { get; set; }
+
+    }
+}
